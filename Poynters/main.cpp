87a9c4917b04636e56d01a341af5686f1ea99d86
Toro_rt49@itstepru.onmicrosoft.com <https://github.com/RomanToropov>
2021-09-09 19:13:24 +0700

    Add project files.

---
+++ Poynters/main.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+//#define POINTERS_BASICS
+#define POINTERS_AND_ARRAYS
+
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+#ifdef POINTERS_BASICS
+	int a = 2;
+	int* pa = &a;
+	cout << a << endl;// вывод значения переменной "а" на экран.
+	cout << &a << endl;//Взятие адреса переменной 'а' прямо при выводе.
+	cout << pa << endl;
+	cout << *pa << endl;
+	int* pb;
+
+	int b = 3;
+	*pb = &b;
+	cout << b << endl;
+	cout << pb << endl;
+	cout << *pb << endl;
+#endif // POINTERS_BASICS
+
+#ifdef POINTERS_AND_ARRAYS
+	const int n = 5;
+	int arr[n] = { 3,5,8,13,21 };
+	cout << arr << endl;
+	cout << *arr << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(arr + i) << "\t";
+	}
+	cout << endl;
+#endif // POINTERS_AND_ARRAYS
+
+}
